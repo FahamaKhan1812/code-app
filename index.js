616fd0edf9f2d0771e9081ebe5e8c99deb360f98
Fahama Khan <fahamakhan990@gmail.com>
2023-02-05 15:47:25 +0500

    update file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/app", (req, res) => {
+app.get("/lab14", (req, res) => {
   res.format({
     text: function () {
       res.send(
@@ -83,77 +83,77 @@ app.get("/app", (req, res) => {
                 LAB#14-TASK NO 2
             -------------------------------
             #include <iostream>
-#include <sstream>
-#include <stack>
-#include <string>
-#include <vector>
-using namespace std;
+            #include <sstream>
+            #include <stack>
+            #include <string>
+            #include <vector>
+            using namespace std;
 
 
 
-vector<string> infix_to_statements(const string &expression) {
-    vector<string> statements;
-    stack<string> operand_stack;
-    stack<string> operator_stack;
-    istringstream iss(expression);
-    string token;
-    while (iss >> token) {
-        if (token == "+" || token == "-" || token == "*" || token == "/") {
-            operator_stack.push(token);
-        } else if (token == "(") {
-            continue;
-        } else if (token == ")") {
-            string operand2 = operand_stack.top();
-            operand_stack.pop();
-            string operand1 = operand_stack.top();
-            operand_stack.pop();
-            string op = operator_stack.top();
-            operator_stack.pop();
-            string statement = operand1 + " " + op + " " + operand2 + ";";
-            statements.push_back(statement);
-            operand_stack.push("result");
-        } else {
-            operand_stack.push(token);
-        }
-    }
-    return statements;
-}
+            vector<string> infix_to_statements(const string &expression) {
+                vector<string> statements;
+                stack<string> operand_stack;
+                stack<string> operator_stack;
+                istringstream iss(expression);
+                string token;
+                while (iss >> token) {
+                    if (token == "+" || token == "-" || token == "*" || token == "/") {
+                        operator_stack.push(token);
+                    } else if (token == "(") {
+                        continue;
+                    } else if (token == ")") {
+                        string operand2 = operand_stack.top();
+                        operand_stack.pop();
+                        string operand1 = operand_stack.top();
+                        operand_stack.pop();
+                        string op = operator_stack.top();
+                        operator_stack.pop();
+                        string statement = operand1 + " " + op + " " + operand2 + ";";
+                        statements.push_back(statement);
+                        operand_stack.push("result");
+                    } else {
+                        operand_stack.push(token);
+                    }
+                }
+                return statements;
+            }
 
-string statement_to_assembly(string statement) {
-    string assembly;
-    istringstream iss(statement);
-    string op1, op, op2;
-    iss >> op1 >> op >> op2;
+            string statement_to_assembly(string statement) {
+                string assembly;
+                istringstream iss(statement);
+                string op1, op, op2;
+                iss >> op1 >> op >> op2;
 
-    if (op == "+") {
-        assembly = "movl " + op1 + ", %eax\n";
-        assembly += "addl " + op2 + ", %eax\n";
-    } else if (op == "-") {
-        assembly = "movl " + op1 + ", %eax\n";
-        assembly += "subl " + op2 + ", %eax\n";
-    } else if (op == "*") {
-        assembly = "movl " + op1 + ", %eax\n";
-        assembly += "imull " + op2 + ", %eax\n";
-    } else if (op == "/") {
-        assembly = "movl " + op1 + ", %eax\n";
-        assembly += "movl " + op2 + ", %ebx\n";
-        assembly += "cltd\n";
-        assembly += "idivl %ebx\n";
-    }
-    return assembly;
-}
+                if (op == "+") {
+                    assembly = "movl " + op1 + ", %eax\n";
+                    assembly += "addl " + op2 + ", %eax\n";
+                } else if (op == "-") {
+                    assembly = "movl " + op1 + ", %eax\n";
+                    assembly += "subl " + op2 + ", %eax\n";
+                } else if (op == "*") {
+                    assembly = "movl " + op1 + ", %eax\n";
+                    assembly += "imull " + op2 + ", %eax\n";
+                } else if (op == "/") {
+                    assembly = "movl " + op1 + ", %eax\n";
+                    assembly += "movl " + op2 + ", %ebx\n";
+                    assembly += "cltd\n";
+                    assembly += "idivl %ebx\n";
+                }
+                return assembly;
+            }
 
-int main() {
-    string expression = "( a - b ) + ( a - c ) + ( a - c )";
-    vector<string> statements = infix_to_statements(expression);
+            int main() {
+                string expression = "( a - b ) + ( a - c ) + ( a - c )";
+                vector<string> statements = infix_to_statements(expression);
 
-    for (vector<string>::iterator it = statements.begin(); it != statements.end(); it++) {
-        cout << *it << endl;
-        cout << statement_to_assembly(*it) << endl;
-       
-    }
-    return 0;
-}
+                for (vector<string>::iterator it = statements.begin(); it != statements.end(); it++) {
+                    cout << *it << endl;
+                    cout << statement_to_assembly(*it) << endl;
+                
+                }
+                return 0;
+            }
 
 
 
